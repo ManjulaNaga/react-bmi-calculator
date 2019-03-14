b2410496569796aae3e5a8fe507fa11176829ff0
ManjulaNaga <cnagamanju@gmail.com>
2019-03-14 15:55:55 -0700

    updated bmi formula

--- src/App.js
+++ src/App.js
@@ -4,31 +4,32 @@ import Range from './Range';
 import Output from './Output';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      height : 170,
-      weight : 65,
-      bmi : 22.49,
-      bmiClass : 'Normal'
+      height: 170,
+      weight: 65,
+      bmi: 22.49,
+      bmiClass: 'Normal'
     }
   }
-  heightChange(height){
+  heightChange(height) {
     this.setState({
-      height:height
-    },this.setBmi);
+      height: height
+    }, this.setBmi);
   }
-  weightChange(weight){
+  weightChange(weight) {
     this.setState({
-      weight:weight
-    },this.setBmi);
+      weight: weight
+    }, this.setBmi);
   }
-  setBmi(){
-    let bmi = ((this.state.weight / this.state.height / this.state.height) * 10000).toFixed(2);
+  setBmi() {
+    let bmi = ((this.state.weight / (this.state.height * this.state.height) * 10000).toFixed(2);
     this.setState({
-      bmi:bmi,
-      bmiClass:this.getBmiClass(bmi)},
-      function(){
+      bmi: bmi,
+      bmiClass: this.getBmiClass(bmi)
+    },
+      function () {
         console.log(this.state);
       });
   }
@@ -39,33 +40,33 @@ Underweight = <18.5
 Normal weight = 18.5–24.9
 Overweight = 25–29.9
 Obesity = BMI of 30 or greater*/
-  getBmiClass(bmi){
-    if(bmi<= 18.5){
+  getBmiClass(bmi) {
+    if (bmi <= 18.5) {
       return 'Underweight';
     }
-    else if(bmi > 18.5 && bmi <=24.9){
-        return 'Normal';
+    else if (bmi > 18.5 && bmi <= 24.9) {
+      return 'Normal';
     }
-    else if(bmi>24.9 && bmi <=29.9){
-          return 'Overweight';
-        }
-    else{
-          return 'Obese';
+    else if (bmi > 24.9 && bmi <= 29.9) {
+      return 'Overweight';
+    }
+    else {
+      return 'Obese';
     }
   }
   render() {
     return (
       <div className="App">
         <div className="box">
-        <h1>BMI Calcular</h1>
-        <form>
-        Height : <br/>
-        <Range onChange ={this.heightChange.bind(this)}/>
-        Weight :<br/>
-        <Range onChange ={this.weightChange.bind(this)}/>
-      </form>
-            <Output data = {this.state}/>
-      </div>
+          <h1>BMI Calcular</h1>
+          <form>
+            Height : <br />
+            <Range onChange={this.heightChange.bind(this)} />
+            Weight :<br />
+            <Range onChange={this.weightChange.bind(this)} />
+          </form>
+          <Output data={this.state} />
+        </div>
       </div>
     );
   }
